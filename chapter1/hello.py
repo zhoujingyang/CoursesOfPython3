e74d8d9d0931cc914ea2dece04e49bce45e17cc8
zhoujingyang <zjy42197393@163.com>
2018-02-02 20:36:10 +0800

    add chapter 1

--- chapter1/hello.py
+++ chapter1/hello.py
@@ -4,14 +4,17 @@ Spyder Editor
 
 This is a temporary script file.
 """
-import sys
 
+"""
+hello,world是学习一门语言第一个要写的程序，就是“你好，世界”的意思
+一般采用打印的方式来进行。
+也验证了环境的可运行。
+"""
+print ("hello，world");
+
+import sys
+#查看版本号
 print (sys.version)
 
-print ("hello");
-
-
-print (1+1)  
-    
 
 
